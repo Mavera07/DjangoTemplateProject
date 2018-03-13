61a5b64fea45102db9a29392652019f826b5719c
Mavera07 <cyberoyal@windowslive.com>
2018-03-14 00:34:04 +0300

    rename study 3

--- setup/rename/rename.py
+++ setup/rename/rename.py
@@ -1,19 +1,21 @@
+import sys
+import os
 
+def replaceTextInDjangoFiles(projpath, oldtext, newtext):
 
-def replaceProjectName(path, projname):
-    pass
+    xfiles = [];xdirs = [];
+    for (dirpath, dirnames, filenames) in os.walk(projpath):
+        xfiles.extend(filenames)
+        xdirs.extend(dirnames)
+        if(".git" in xdirs): xdirs.remove(".git")
+        break
 
-def replaceApplicationName():
-    pass
+    for xfile in xfiles:
+        replaceTextInFile(projpath+"/"+xfile, oldtext, newtext)
 
-def replaceTextInDjangoFiles(path):
-    focusFiles = [];focusDirs = [];
-    for (dirpath, dirnames, filenames) in os.walk(path):
-        focusFiles.extend(filenames)
-        focusDirs.extend(dirnames)
-        break    
+    for xdir in xdirs:
+        replaceTextInDjangoFiles(projpath+"/"+xdir, oldtext, newtext)
 
-    pass
 
 def replaceTextInFile(file_absolute_path, oldtext, newtext):
     with open(file_absolute_path, 'r') as f:
@@ -23,4 +25,17 @@ def replaceTextInFile(file_absolute_path, oldtext, newtext):
 
     with open(file_absolute_path, 'w') as f:
         f.write(filedata)
+
+def replaceApplicationName(projpath, newappname):
+    os.rename(projpath+"/DjangoTemplateApplication",projpath+"/"+newappname)
+
+def replaceProjectName(projpath, newprojname):
+    parentpath = os.path.abspath(os.path.join(projpath, os.pardir))
+    os.rename(projpath+"/DjangoTemplateProject",projpath+"/"+newprojname)
+    os.rename(projpath, parentpath+"/"+newprojname)
+
+replaceTextInDjangoFiles(sys.argv[1], "DjangoTemplateProject" ,sys.argv[2])
+replaceTextInDjangoFiles(sys.argv[1], "DjangoTemplateApplication" ,sys.argv[3])
+replaceApplicationName(sys.argv[1], sys.argv[3])
+replaceProjectName(sys.argv[1], sys.argv[2])
 
