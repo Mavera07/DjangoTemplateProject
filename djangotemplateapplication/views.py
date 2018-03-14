fe4d297a23c86e5fe26d623b7882a9e641c0d821
Mavera07 <cyberoyal@windowslive.com>
2018-03-15 00:49:30 +0300

    index view is implemented

--- djangotemplateapplication/views.py
+++ djangotemplateapplication/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+import yaml
+import os
+
+def index(request):
+    currentfilepath = os.path.dirname(os.path.abspath(__file__))
+    projpath = nthParent(currentfilepath,1)
+    config = yaml.safe_load(open(projpath+"/resources/config.yml"))
+
+    return HttpResponse("Hello, world. You're at the polls index."+config["xvar"])
+
+def nthParent(path,n):
+    result = os.sep.join(path.split(os.sep)[:-n])
+    return result
