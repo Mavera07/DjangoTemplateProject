2af71c466a1f5a42ccf6f76d56c9e59aa86ab798
Ubuntu <ubuntu@ip-172-31-12-34.us-west-2.compute.internal>
2018-03-28 09:59:56 +0000

    base and login preparation, migrations volume map, vendor creation with sorttable.js

--- setup/docker/start.sh
+++ setup/docker/start.sh
@@ -1,6 +1,7 @@
 docker volume create --name djangotemplateproject_postgresql-volume
-docker build -f ./Dockerfile-postgres -t djangotemplateproject_postgres_i ../../../djangotemplateproject
+docker volume create --name djangotemplateproject_migrations-volume
 
+docker build -f ./Dockerfile-postgres -t djangotemplateproject_postgres_i ../../../djangotemplateproject
 docker build -f ./Dockerfile-python -t djangotemplateproject_i ../../../djangotemplateproject
 
 docker-compose up -d
