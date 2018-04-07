62aafac8811829996fb202f9f7a38e8cfdbdbd2f
Mavera07 <cyberoyal@windowslive.com>
2018-04-07 12:13:54 +0300

    docker structure improvement - init django implementation - step 1

--- setup/docker/volume.sh
+++ setup/docker/volume.sh
@@ -4,8 +4,10 @@ docker volume create --name djangotemplateproject_bower-volume
 
 docker build -f ./init/Dockerfile-init-postgres -t djangotemplateproject_init_postgres_i ../../../djangotemplateproject
 docker build -f ./init/Dockerfile-init-bower -t djangotemplateproject_init_bower_i ../../../djangotemplateproject
+docker build -f ./init/Dockerfile-init-django -t djangotemplateproject_init_django_i ../../../djangotemplateproject
 
 docker-compose -f ./init/docker-compose-init.yml up -d
 
 docker rm -f djangotemplateproject_init_bower_c
 docker rm -f djangotemplateproject_init_postgres_c
+docker rm -f djangotemplateproject_init_django_c
