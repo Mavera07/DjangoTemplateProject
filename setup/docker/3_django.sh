b3ba616caa3eacca4d8802f700f60f2a38766ede
sahinbatmaz <sahin.batmaz@gmail.com>
2018-04-24 01:51:39 +0300

    Development volume implementation

--- setup/docker/3_django.sh
+++ setup/docker/3_django.sh
@@ -2,4 +2,8 @@ DIR=$PWD/`dirname $0`
 
 docker build -f $DIR/run/Dockerfile-django -t djangotemplateproject_django_i $DIR/../../../djangotemplateproject
 
+docker rm -f djangotemplateproject_django_c
+docker volume rm -f djangotemplateproject_development-volume || true
+docker volume create --name djangotemplateproject_development-volume
+
 docker-compose -f $DIR/run/docker-compose-run-django.yml up -d
