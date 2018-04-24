72efbb48c8c683dff5c78b0c0d562ff24ce6ab89
sahinbatmaz <sahin.batmaz@gmail.com>
2018-04-24 21:02:15 +0300

    remove django-bower

--- djangotemplateproject/settings.py
+++ djangotemplateproject/settings.py
@@ -46,13 +46,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-
-    'djangobower',
 ]
-
-BOWER_INSTALLED_APPS = (
-    'jquery',
-)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -148,6 +142,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, 'djangotemplateapplication/static')
 
 MIGRATION_MODULES = {'djangotemplateapplication': 'migrations'}
